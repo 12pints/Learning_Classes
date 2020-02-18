1b7e9e62023a6a56a68ee7db3af6df1308e5e3c0
dmink <12pints@gmail.com>
2020-02-18 16:13:17 +1100

    changed the Dictionaries_examples_2.py file with some file operations ,w

--- Dictionaries_examples_2.py
+++ Dictionaries_examples_2.py
@@ -1,26 +1,35 @@
-#let write some script that goes over some text and counts all words to see how often the indivudually occur
+#working Feb 18 2020
+#let write some script that goes over some text and counts all words to see how often the individually occur
 
-myString="Schonis heeft tijdens een debat over ’duurzaam vervoer’ staatssecretaris Van Veldhoven, gevraagd om te gaan praten met de branche. Hij ziet graag dat de populaire bestelwebsites als Bol.com en Zalando aan klanten laten zien hoeveel CO2-uitstoot er gepaard gaat met een rit."
+myString="Schonis heeft tijdens een debat over 'duurzaam vervoer' staatssecretaris Van Veldhoven, gevraagd om te gaan praten met de branche. Hij ziet graag dat de populaire bestelwebsites als Bol.com en Zalando aan klanten laten zien hoeveel CO2-uitstoot er gepaard gaat met een rit."
 
-#first thing we will need to do is remove the text from any garbage, so we can turn it into a dictionary
-#each work is a dictionary key, with a value: its occurence
-myString=myString.replace(".","")  #remove periods
-myString=myString.replace(",","")  #remove commas
-myString=myString.replace("'","")  #remove capostrophies
-myString=myString.lower()  #turn into lower case
+# first thing we will need to do is remove the text from any garbage, so we can turn it into a dictionary
+# each word is a dictionary key, with a value: its occurrence
+myString=myString.replace(".","")  # remove periods
+myString=myString.replace(",","")  # remove commas
+myString=myString.replace("'","")  # remove apostrophies
+myString=myString.lower()  # turn into lower case
 
-mySplitString=myString.split()  #split by spaces
-#so now you actually have a list containing all the words
-# print(mySplitString)    <----can be used to test a preliminary result
+mySplitString=myString.split()  # split by spaces
+# so now you actually have a list containing all the words
+print(mySplitString)   # <----can be used to test a preliminary result
+print(type(mySplitString))  # <----this shows its now a list
 
-#now lets create a dictionary
+# now lets create a dictionary
 
-wordDictionary={}   #create emoty dictionary
-for word in mySplitString:    #For each word in splitString, iterate over it
-    if word in wordDictionary:     #if the word is already in the dictionary, add 1 to the value, cos its found again
+wordDictionary={}   # create empty dictionary
+for word in mySplitString:    # For each word in splitString list, iterate over it
+    if word in wordDictionary:     # if the word is already in the dictionary, add 1 to the value, cos its found again
         wordDictionary[word] +=1
     else:
-        wordDictionary[word]=1     #if the works is not YET in the dictionary, make count 1, cos its new
+        wordDictionary[word]=1     # if the works is not YET in the dictionary, make count 1, cos its new
 
 print(wordDictionary)
+
+# if we wanted to write that to a file:
+
+out_filename='Dictionaries_examples_2_output.txt'
+f=open(out_filename,'w')
+print(wordDictionary, file=f)
+f.close()
 
