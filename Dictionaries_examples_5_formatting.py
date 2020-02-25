50cdeb0b57d680f6b150e50d573ab5ab3c690317
dmink <12pints@gmail.com>
2020-02-25 18:40:46 +1100

    added Panda df code to dict formatting 5

--- Dictionaries_examples_5_formatting.py
+++ Dictionaries_examples_5_formatting.py
@@ -1,26 +1,22 @@
-# this code has a dictionary with keys and values and writes them to a file in culumns
-# no working yet feb 19 2020
-# Dennis Mink
+# this code has a dictionary with keys and values and writes them to a file in columns
+# partially working, need more args in the data frame creation, to make the car types columns
+# source:  https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.from_dict.html
+# Dennis Mink feb 2020
 
+import pandas as pd
 
-dict={'Citroen': 1, 'Maserati': 1, 'Ferrari': 1, 'BMW': 2, 'RollsRoyce': 1, 'AMGr': 1, 'AstonMartin': 1, 'Audi': 4}
+Cars_dict={'Citroen': [1], 'Maserati': [1], 'Ferrari': [1], 'BMW': [2], 'RollsRoyce': [1], 'AMG': [1], 'AstonMartin': [1], 'Audi': [4]}
+print(type(dict))
+
+# here is where we create a df thu panda, using the from_dict argument
+df=pd.DataFrame.from_dict(Cars_dict)
+
+print(df)
 
 # next we print the dictionary
 out_filename = 'Dictionaries_examples_5_formatting_output.txt'
 f = open(out_filename, 'w')
-print(dict, file=f)
 
-# next we break it up an print each key value pair on a new line (\n),
-# you can see by the (  )  that the key value pairs are now tuples:
-
-for key, value in dict.items():
-     print((key, value), end="\n", file=f)
-
-
-# so how do we now change the into plain colums that we can actually present in like a csv?
-# we will use the items attribute that we apply to a dictionary, using double indent \t\t or tab
-
-for k , v in dict.items():
-        print((k+",", v),file=f)
+print(df, file=f)
 
 f.close()
