e7f0bb563c5715ef31c20dc83b8ac4956b62baf9
dmink <12pints@gmail.com>
2020-02-26 19:04:16 +1100

    , indexed

--- Dictionaries_examples_5_formatting.py
+++ Dictionaries_examples_5_formatting.py
@@ -5,17 +5,28 @@
 
 import pandas as pd
 
+def header(msg):
+    print("-"*50)
+    print("[+msg+]")
+    print("\n")
+
 Cars_dict={'Citroen': [1], 'Maserati': [1], 'Ferrari': [1], 'BMW': [2], 'RollsRoyce': [1], 'AMG': [1], 'AstonMartin': [1], 'Audi': [4]}
 print(type(dict))
 
 # here is where we create a df thu panda, using the from_dict argument
-df=pd.DataFrame.from_dict(Cars_dict)
+# orient=index makes it print indexed in rows (default=column)
+df=pd.DataFrame.from_dict(Cars_dict, orient='index')
+
+# next we print the dictionary, to screen and to file
+
+
+header=("1. Convert a library into a pandas dataframe and print to screen and file")
 
 print(df)
 
-# next we print the dictionary
 out_filename = 'Dictionaries_examples_5_formatting_output.txt'
 f = open(out_filename, 'w')
+
 
 print(df, file=f)
 
